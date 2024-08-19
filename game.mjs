542c5118cfdbd28a49b823cf647adc7c5385306c
Daniel K. Valente <dan.k.valente@gmail.com>
2024-08-19 19:08:00 +0200

    - Implemented meInstance variable for class referencing. - Resolved issue with async operation.

--- game.mjs
+++ game.mjs
@@ -22,19 +22,21 @@ class Game {
 	start() {
 		let selectedNumber = Game.getRandom(0, 100);
 		this.prompt("Pick a number between 0 and 100...");
+		let meInstance=this;
 		this.getInput().then((userResponse) => {
 			console.log(userResponse);
-			this.#usersGuesses.push(userResponse);
+			meInstance.#usersGuesses.push(userResponse);
 			if (userResponse === selectedResponse) {
-				this.points++;
-				this.prompt("Excellent job!!!");
+				meInstance.points++;
+				meInstance.prompt("Excellent job!!!");
 			}
-			this.prompt("Continue? \"Yes\" or \"No\"");
-			let userContinue = this.getInput().toLowerCase();
-			if (userContinue === "yes")
-				start();
-			else
-				this.showResults();
+			meInstance.prompt("Continue? \"Yes\" or \"No\"");
+			meInstance.getInput().then((userContinue) => {
+				if (userContinue === "yes")
+					start();
+				else
+					meInstance.showResults();
+			});
 		});
 	}
 	/**
